112721e3a3bedc3890523116e6d7cdec8148b2fb
Erin Brown <emberbrown@gmail.com>
2019-06-27 00:00:39 -0400

    Add favorites ⭐

---
+++ db/migrate/20190627025940_create_favorites.rb
@@ -0,0 +1,11 @@
+class CreateFavorites < ActiveRecord::Migration[6.0]
+  def change
+    create_table :favorites do |t|
+      t.boolean :public
+      t.references :users, null: false, foreign_key: true
+      t.references :favorable, polymorphic: true, index: true
+
+      t.timestamps
+    end
+  end
+end
