c9fc5d561da814f8f4fb040302a23f133ff680e3
Erin Brown <emberbrown@gmail.com>
2019-06-29 14:09:58 -0400

    Harden some associations

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,22 +4,26 @@ class Work < ApplicationRecord
 
   has_many :work_creators, dependent: :destroy
   has_many :creators, through: :work_creators
-  has_many :reviews
-  has_many :favorites, as: :favorable
-  has_many :links, as: :linkable
+  has_many :reviews, dependent: :destroy
+  has_many :favorites, as: :favorable, dependent: :destroy
+  has_many :links, as: :linkable, dependent: :destroy
 
-  has_many :references_to, as: :reference_to, class_name: 'Reference'
-  has_many :references, as: :referenced_by, class_name: 'Reference'
+  has_many :references_to, as: :reference_to, class_name: 'Reference', dependent: :destroy
+  has_many :references, as: :referenced_by, class_name: 'Reference', dependent: :destroy
 
   has_many :mentioned_creators, through: :references,
-           source: :reference_to, source_type: 'Creator'
+           source: :reference_to, source_type: 'Creator',
+           dependent: :destroy
 
   has_many :mentioned_works, through: :references,
-           source: :reference_to, source_type: 'Work'
+           source: :reference_to, source_type: 'Work',
+           dependent: :destroy
 
   has_many :creator_referrers, through: :references_to,
-           source: :referenced_by, source_type: 'Creator'
+           source: :referenced_by, source_type: 'Creator',
+           dependent: :destroy
 
   has_many :work_referrers, through: :references_to,
-           source: :referenced_by, source_type: 'Work'
+           source: :referenced_by, source_type: 'Work',
+           dependent: :destroy
 end
--- app/models/work_creator.rb
+++ app/models/work_creator.rb
@@ -1,4 +1,7 @@
 class WorkCreator < ApplicationRecord
   belongs_to :work
   belongs_to :creator
+
+  validates_presence_of :work
+  validates_presence_of :creator
 end
